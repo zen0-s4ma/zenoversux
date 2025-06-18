475f99d799bd643fd1485c6ede29dadc5179ae13
zen0-s4ma <nohuddlelol@gmail.com>
2025-06-18 10:40:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,108 +1,100 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# 1. Comprobar que somos root
+# 1. root?
 if [ "$EUID" -ne 0 ]; then
   echo "Por favor, ejecuta este script como root."
   exit 1
 fi
 
-# 2. Habilitar repositorios extra y multilib en /etc/pacman.conf
-sed -i '/^\[extra\]/,/Include/ s/^#//' /etc/pacman.conf
-sed -i '/^\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf
-pacman -Sy  # refrescar base de datos
+# 2. habilitar extra y multilib
+sed -i '/^\[extra\]/,/^Include/ s/^#//' /etc/pacman.conf
+sed -i '/^\[multilib\]/,/^Include/ s/^#//' /etc/pacman.conf
+pacman -Sy
 
-# 3. Actualizar todo el sistema
+# 3. actualizar
 pacman -Syu --noconfirm
 
-# 4. Instalar herramientas de desarrollo básicas y Git
+# 4. base-devel + git
 pacman -S --needed --noconfirm base-devel git
 
-# 5. Instalar paquetes de repositorio oficial
-pacman -S --needed --noconfirm \
+# 5. paquetes oficiales
+PKGS=(
   # Kernel y firmware
-  linux linux-headers linux-firmware intel-ucode kmod \
+  linux linux-headers linux-firmware intel-ucode kmod
   # Wayland + KDE Plasma 6
-  xorg-xwayland plasma-meta kde-applications-meta plasma-wayland-session sddm \
+  xorg-xwayland plasma-meta kde-applications-meta plasma-wayland-session sddm
   # Red y conectividad
-  networkmanager plasma-nm network-manager-applet wpa_supplicant wireless_tools \
-  bluez bluez-utils bluedevil blueman \
-  pipewire pipewire-pulse pipewire-alsa wireplumber alsa-utils pavucontrol \
-  usbutils udisks2 udiskie \
+  networkmanager plasma-nm network-manager-applet wpa_supplicant wireless_tools
+  bluez bluez-utils bluedevil blueman
+  pipewire pipewire-pulse pipewire-alsa wireplumber alsa-utils pavucontrol
+  usbutils udisks2 udiskie
   # Virtualización
-  qemu qemu-full libvirt dnsmasq virt-manager virt-viewer bridge-utils ebtables virt-install \
-  seabios edk2-ovmf \
+  qemu qemu-full libvirt dnsmasq virt-manager virt-viewer bridge-utils ebtables virt-install
+  seabios edk2-ovmf
   # Contenedores
-  lxc lxd docker docker-compose \
-  podman podman-docker cockpit cockpit-podman \
+  lxc lxd docker docker-compose
+  podman podman-docker cockpit cockpit-podman
   # Multimedia
-  ffmpeg gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav \
-  libdvdcss libva-intel-driver libva-vdpau-driver vdpau-driver \
-  # Software usuario
-  flatpak \
+  ffmpeg gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav
+  libdvdcss libva-intel-driver libva-vdpau-driver vdpau-driver
+  # Usuario
+  flatpak
   # Desarrollo
-  python python-pip rust go jdk-openjdk nodejs npm \
-  cmake meson ninja gdb clang \
+  python python-pip rust go jdk-openjdk nodejs npm
+  cmake meson ninja gdb clang
   # Hardware / diagnóstico
-  inxi hwinfo lshw dmidecode pciutils \
-  # CLI de sistema
-  htop fastfetch neofetch glances iotop ncdu btop tmux screen \
-  # Bajo nivel y módulos
-  dkms kexec-tools acpi acpid cpupower \
-  # Utilidades comunes
-  wget curl tar unzip zip rsync git man-db man-pages which sudo chrony systemd-timesyncd gedit \
-  # NVIDIA + optimización gaming
-  nvidia nvidia-utils lib32-nvidia-utils mangohud lib32-mangohud gamescope gamemode \
-  steam lutris wine \
+  inxi hwinfo lshw dmidecode pciutils
+  # CLI
+  htop fastfetch neofetch glances iotop ncdu btop tmux screen
+  # Módulos y bajo nivel
+  dkms kexec-tools acpi acpid cpupower
+  # Utilidades
+  wget curl tar unzip zip rsync git man-db man-pages which sudo chrony systemd-timesyncd gedit
+  # Gaming / NVIDIA
+  nvidia nvidia-utils lib32-nvidia-utils mangohud lib32-mangohud gamescope gamemode
+  steam lutris wine
   # Seguridad / sandbox
-  nsjail \
+  nsjail
   # Snapshots
-  snapper \
+  snapper
   # GRUB y herramientas
   grub efibootmgr os-prober
+)
 
-# 6. Instalar xen desde AUR (no está en repos oficiales)
+pacman -S --needed --noconfirm "${PKGS[@]}"
+
+
+# 6. xen (AUR)
 cd /tmp
 git clone https://aur.archlinux.org/xen.git
-cd xen
-makepkg -si --noconfirm
-cd /
-rm -rf /tmp/xen
+cd xen && makepkg -si --noconfirm
+cd / && rm -rf /tmp/xen
 
-# 7. Instalar AUR helper (paru)
+# 7. paru (AUR helper)
 cd /tmp
 git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si --noconfirm
-cd /
-rm -rf /tmp/paru
+cd paru && makepkg -si --noconfirm
+cd / && rm -rf /tmp/paru
 
-# 8. Instalar paquetes AUR opcionales
-paru -S --noconfirm --needed \
+# 8. AUR opcionales
+paru -S --needed --noconfirm \
   aqemu snapper-gui btrfs-assistant grub-customizer podman-desktop heroic-games-launcher-bin
 
-# 9. Configurar Flatpak y aplicaciones de Flathub
+# 9. Flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y \
-  flathub org.libreoffice.LibreOffice \
-  flathub org.videolan.VLC \
-  flathub tv.kodi.Kodi \
-  flathub org.librewolf.Librewolf \
-  flathub com.visualstudio.code
+  org.libreoffice.LibreOffice org.videolan.VLC tv.kodi.Kodi \
+  org.librewolf.Librewolf org.gimp.GIMP com.visualstudio.code
 
-# 10. Habilitar y arrancar servicios systemd
-systemctl enable \
-  sddm NetworkManager bluetooth libvirtd lxd docker \
-  snapper-timeline.timer snapper-cleanup.timer
+# 10. servicios
+systemctl enable sddm NetworkManager bluetooth libvirtd lxd docker \
+               snapper-timeline.timer snapper-cleanup.timer
+systemctl start  sddm NetworkManager bluetooth libvirtd lxd docker \
+               snapper-timeline.timer snapper-cleanup.timer
 
-systemctl start \
-  sddm NetworkManager bluetooth libvirtd lxd docker \
-  snapper-timeline.timer snapper-cleanup.timer
-
-echo -e "\n¡Instalación completada! Reinicia el sistema para cargar todo correctamente."
-
-# Esperar que el usuario presione una tecla
-read -n 1 -s -r -p "Presiona cualquier tecla para reiniciar..."
-
-# Reiniciar el sistema
+# 11. pausa y reboot
+echo -e "\n¡Instalación completada!"
+read -n1 -s -r -p "Presiona cualquier tecla para reiniciar..."
+echo
 reboot
